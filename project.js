de4918c001782ae4f031edf65756d4cd15076e6f
saran0119 <saran321sriram@gmail.com>
2025-12-28 23:13:50 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,19 @@
+ function showMessage() {
+        alert("Good governance with good intentions is the hallmark of our government.");
+    }
+
+const buttn = document.getElementsByClassName("buttn")
+
+buttn.addEventListner("click",showmes)
+    function showmes() {
+        alert("Since 2014, Prime Minister Narendra Modi and President Vladimir Putin have developed a robust and enduring friendship grounded in strategic cooperation, mutual respect, and shared national interests, shaping a \"special and privileged strategic partnership\" between India and Russia.")
+    }
+
+function showme() {
+
+    document.getElementById("text").innerHTML="The Indian government demonetisation of ₹500 and ₹1,000 banknotes in 2016 aimed to curb the shadow economy and reduce the use of illicit and counterfeit cash.The ₹2000 banknote was withdrawn from circulation in 2018-19, and the ₹500 note remains valid and continues to be used nationwide for all transactions"
+    
+    }
+
+
+   
